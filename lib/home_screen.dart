520fe5d3e182fec8e9daacba300059ef0c6e1f31
sinemakca <sinemmakcaa@icloud.com>
2023-12-28 04:08:03 +0300

    arrow back icon

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:virtual_shopping_app/items_upload_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,18 +15,20 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Virtual Shop", //burayı değiştirip marka adı koyulacak
+
             style: TextStyle(
 
               fontSize: 18,
               letterSpacing: 2,
               fontWeight: FontWeight.bold,
+              color: Colors.pink,
             )
             ),
         actions: [
           IconButton(
             onPressed: ()
             {
-
+              Navigator.push(context, MaterialPageRoute(builder: (c)=> ItemsUploadScreen ()));
             },
             icon: const Icon(
               Icons.add,
--- lib/items_upload_screen.dart
+++ lib/items_upload_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-class ItemsUploadScreen extends StatefulWidget {
-  const ItemsUploadScreen({super.key});
-
+class ItemsUploadScreen extends StatefulWidget
+{
   @override
   State<ItemsUploadScreen> createState() => _ItemsUploadScreenState();
 }
@@ -12,20 +11,37 @@ class _ItemsUploadScreenState extends State<ItemsUploadScreen>
 {
   // upload form screen
 
-  Widget build(BuildContext context)
+  Widget uploadFormScreen()
   {
     return Scaffold(
-      backgroundColor: Colors.purpleAccent,
+      backgroundColor: Colors.black54,
       appBar:  AppBar(
+        backgroundColor: Colors.black54,
         title:  const Text(
           "Upload New Item",
           style: TextStyle(
-            color: Colors.black,
+            color: Colors.white,
           ),
         ),
         centerTitle: true,
+        leading: IconButton(
+          onPressed: ()
+          {
+            Navigator.pop(context);
+          },
+          icon:  const Icon(
+            Icons.arrow_back_rounded,
+            color: Colors.pink,
+          ),
+        ),
       ),
     );
 
   }
+  @override
+  Widget build(BuildContext context)
+  {
+    return uploadFormScreen();
+  }
+
 }
